69bb0c017329ddfcc5d40504900bc1a279604a7d
Dave Loyst <david.loyst@gmail.com>
2016-07-18 22:59:29 -0700

    change bar chart template component to stateless functional component

--- src/demo/components/d3Components/BarChart.js
+++ src/demo/components/d3Components/BarChart.js
@@ -3,22 +3,36 @@ import d3DataToJSX from './../../../react-d3/d3DataToJSX';
 import ChildComponent from './ChildComponent';
 import createBarChart from './../../basic-graphs/createBarChart';
 
-export default class extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {d3DOM: [], state: []};
-  }
+// export default class extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {d3DOM: [], state: []};
+//   }
 
-  componentWillReceiveProps(nextProps) {
-    let d3Data = d3DataToJSX(createBarChart(nextProps.data));
-    this.setState({d3DOM: d3Data.mappedData, state: d3Data.state})
-  }
+//   componentWillReceiveProps(nextProps) {
+//     let d3Data = d3DataToJSX(createBarChart(nextProps.data));
+//     this.setState({d3DOM: d3Data.mappedData, state: d3Data.state})
+//   }
 
-  render() {
-    return (
-      <div>
-        <ChildComponent data={this.state} />
-      </div>
-    )
-  }
-};
+//   render() {
+//     return (
+//       <div>
+//         <ChildComponent data={this.state} />
+//       </div>
+//     )
+//   }
+// };
+
+// import React from 'react';
+// import d3DataToJSX from './../react-d3/d3DataToJSX';
+// import ChildComponent from './ChildComponent';
+// import createBarChart from './../charts/createBarChart';
+
+export default (node) => {
+  let d3Data = d3DataToJSX(createBarChart(node.data));
+  return (
+    <div>
+      <ChildComponent data={{ d3DOM: d3Data.mappedData, state: d3Data.state }} />
+    </div>
+  )
+}
